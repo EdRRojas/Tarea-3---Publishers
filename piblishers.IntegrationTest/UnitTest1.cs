1f90436bbb35badb657ee7b75e2d84af59a30d80
EdRRojas <20220847@itla.edu.do>
2024-02-04 14:44:33 -0400

    Creacion de la estructura del proyecto.

---
+++ piblishers.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace piblishers.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- publishers.UnitTest/obj/Debug/net6.0/publishers.UnitTest.AssemblyInfo.cs
+++ publishers.UnitTest/obj/Debug/net6.0/publishers.UnitTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("publishers.Domain")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("publishers.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4efb7956437a19640eb28b6cefaef487e8547e94")]
-[assembly: System.Reflection.AssemblyProductAttribute("publishers.Domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("publishers.Domain")]
+[assembly: System.Reflection.AssemblyProductAttribute("publishers.UnitTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("publishers.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
