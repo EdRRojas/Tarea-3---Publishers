7bcb854a2f6d5457d1645c2eef0be4afc0bbca56
EdRRojas <20220847@itla.edu.do>
2024-02-11 10:57:21 -0400

    Creacion de las entidades en la capa de Domain.

--- publishers.Domain/Entities/Titles.cs
+++ publishers.Domain/Entities/Titles.cs
@@ -2,6 +2,15 @@
 {
     public class Titles
     {
-
+        public string? title_id {  get; set; }
+        public string? title { get; set; }
+        public string? type { get; set; }
+        public string? pub_id { get; set; }
+        public decimal price { get; set; }
+        public decimal advance {  get; set; }
+        public int royalty { get; set; }
+        public int ytd_sales { get; set; }
+        public string? notes { get; set; }
+        public DateTime pubdate { get; set; }
     }
 }
