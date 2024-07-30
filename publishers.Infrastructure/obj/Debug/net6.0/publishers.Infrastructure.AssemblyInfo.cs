08671260136bf397866b7330defdfd3f4657d069
EdRRojas <20220847@itla.edu.do>
2024-07-29 22:33:26 -0400

    Proyecto original sin cambios

--- publishers.Infrastructure/obj/Debug/net6.0/publishers.Infrastructure.AssemblyInfo.cs
+++ publishers.Infrastructure/obj/Debug/net6.0/publishers.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("publishers.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b59d1cd69d839383e4c96451f821249b690d2c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46a8079a6d555fe790ca8dfc12eb88fdd2705fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("publishers.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("publishers.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
